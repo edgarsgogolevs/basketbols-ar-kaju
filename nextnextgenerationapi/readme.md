be5c413b5410bcc0f9e63311c6d956093d2b2777
Edgars <edgarsgogolevs@gmail.com>
2023-12-03 20:55:24 +0200

    work work work

--- nextnextgenerationapi/readme.md
+++ nextnextgenerationapi/readme.md
@@ -1,17 +1,21 @@
 ### Run locally
 
+! On UNIX like platforms `unixodbc` should be installed. !
+
 1. Create Python 3.10 virtual environment
 For example:
 ```bash
 pyenv install 3.10
+python -m venv env
+source env/bin/activate
 ```
 
-1. Install dependencies
+2. Install dependencies
 ```bash
 pip install -r requirements.txt
 ```
 
-2. For better dev experience install dev dependencies
+3. For better dev experience install dev dependencies
 ```bash
 pip install -r requirements.dev.txt
 ```
--- nextnextgenerationapi/source/api/teams.py
+++ nextnextgenerationapi/source/api/teams.py
@@ -3,17 +3,22 @@ import logging
 from flask_apispec import marshal_with, use_kwargs  # type: ignore
 
 from schemas.TeamSchema import TeamSchema
+from modules.db import Db
 
+TEAMS_TABLE = "basketball.teams"
 
 lg = logging.getLogger("api.teams")
 bp = Blueprint("teams", __name__, url_prefix="/teams")
-
+db = Db()
 
 @bp.route("/all", methods=["GET"])
 @marshal_with(TeamSchema(many=True))
 def get_all_teams():
-    # TODO: implement
-    return jsonify({"teams": "all"})
+    data = db.select(f"SELECT * FROM {TEAMS_TABLE}")
+    if not data:
+        return {"error": "Not found"}, 404
+    ret = TeamSchema(many=True).dump(data)
+    return jsonify(ret)
 
 
 @bp.route("/<int:team_id>", methods=["GET"])
