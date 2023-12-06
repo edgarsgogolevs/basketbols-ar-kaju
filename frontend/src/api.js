d6d2c1d4cac555d885cdd94b46c435a15dd7000d
Daniils_Seliverstovs <seliverstovsdaniils@gmail.com>
2023-12-06 23:19:23 +0200

    Update API base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default () => {
     const http = axios.create({
-        baseURL: "http://localhost:8069/"
+        baseURL: "basketball-api.happybush-f14b687c.westeurope.azurecontainerapps.io"
 
     });
     return http;
