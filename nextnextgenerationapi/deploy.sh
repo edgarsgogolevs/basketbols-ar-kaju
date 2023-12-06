7342d351ca870691e66e95c200d75d6cc6d80f33
Edgars <edgarsgogolevs@gmail.com>
2023-12-06 23:25:43 +0200

    azure deploy script

---
+++ nextnextgenerationapi/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source .azenv
+az containerapp create \
+  --name $API_NAME \
+  --resource-group $RESOURCE_GROUP \
+  --environment $ENVIRONMENT \
+  --image $ACR_NAME.azurecr.io/$API_NAME:latest \
+  --target-port $PORT \
+  --env-vars $AZURE_ENV_VARS \
+  --ingress 'external' \
+  --registry-server $ACR_NAME.azurecr.io \
+  --query properties.configuration.ingress.fqdn
