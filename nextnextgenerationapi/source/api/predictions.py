44bb240e76ab2763b3ae75fa45683c0afba60f7d
Edgars <edgarsgogolevs@gmail.com>
2023-12-17 18:44:48 +0200

    some more endpoints

--- nextnextgenerationapi/source/api/predictions.py
+++ nextnextgenerationapi/source/api/predictions.py
@@ -4,11 +4,13 @@ from flask_apispec import marshal_with, use_kwargs  # type: ignore
 
 from modules.predict import predict_game_by_game_id_model_id # type: ignore
 import modules.validate as validate
+from modules.db import Db
 
 from schemas.GamePredictionSchema import GamePredictionSchema, GamePredictionRequestSchema # type: ignore
 
 bp = Blueprint("predictions", __name__, url_prefix="/predictions")
 lg = logging.getLogger("api.predictions")
+db = Db()
 
 
 @bp.route("/predict_game/<int:game_id>/<int:model_id>", methods=["GET"])
@@ -26,12 +28,14 @@ def predict_game(game_id: int, model_id: int):
 
 @bp.route("/predict_games/<int:model_id>", methods=["GET"])
 @marshal_with(GamePredictionSchema(many=True))
-def get_models_predicted_games():
+def get_models_predicted_games(model_id: int):
     """
     List of predicted games by model.
     """
-    # TODO: Implement
-    return "List of predicted games"
+    data = db.select("SELECT * FROM basketball.game_predictions WHERE model_id = ?", (model_id,))
+    if not data:
+        return {"error": "Not found"}, 404
+    return data
 
 
 # @bp.route("/<string:model>", methods=["GET"])
