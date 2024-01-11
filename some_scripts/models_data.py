8b91478fb87db3155bf0481b1ed7989544338387
Edgars <edgarsgogolevs@gmail.com>
2024-01-11 22:39:32 +0200

    some_scripts/

---
+++ some_scripts/models_data.py
@@ -0,0 +1,56 @@
+data = [
+    {
+        "name": "Yuri Pavlovich Sokolov",
+        "description": "Yuri Pavlovich Sokolov, dubbed the 'smartest man on earth,' resides in a modest Soviet-era apartment. Despite his impoverished surroundings, Yuri's brilliance shines. An alcoholic genius, he spends nights poring over complex theories, vodka in hand, predicting NBA game outcomes under the dim light of a solitary lamp.",
+        "profile_picture": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Yuri_Pavlovich_Sokolov.png",
+        "picture_small": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Yuri_Pavlovich_Sokolov_small.png",
+        "nominal_precision": 0.55,
+        "path": "svc"
+    },
+    {
+        "name": "Nostradamus",
+        "description": "In a candle-lit chamber, Nostradamus, with intense focus, scribbled furiously. His forehead veins bulged as he envisioned an NBA game, foreseeing intricate plays and unexpected outcomes. Amidst scattered scrolls, his quill danced, predicting the victor in a whirl of ink, merging mystic foresight with the pulse of modern sports.",
+        "profile_picture": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Nostradamus.png",
+        "picture_small": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Nostradamus_small.png",
+        "nominal_precision": 0.6,
+        "path": "random_forest"
+    },
+    {
+        "name": "Jānis Ozoliņš",
+        "description": "Jānis Ozoliņš, a seasoned Latvian farmer with a secret passion for basketball, gazed at the setting sun and pondered the upcoming NBA game. He had a hunch. \"Tonight,\" he mused, \"the underdogs will triumph against all odds.\" His intuition, honed by years of working with nature, rarely failed him. As the game unfolded on his old radio, Jānis listened intently, a small smile playing on his lips as his prediction came true, reminding him that sometimes, the most unexpected outcomes are the most thrilling.",
+        "profile_picture": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Janis_Ozolins.png",
+        "picture_small": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Janis_Ozolins_small.png", 
+        "nominal_precision": 0.65,
+        "path": "logistic_regression"
+    },
+    {
+        "name": "Jordan Basketfield",
+        "description": "At just 15, Jordan Basketfield was already a legend in his hometown. Known as the \"Basket Prodigy,\" he had an uncanny ability to predict NBA game outcomes with astounding accuracy. His bedroom, a shrine to basketball, buzzed with the latest stats and odds. Despite his youth, Jordan's insights were sought after by basketball enthusiasts nationwide, making him a rising star in the sports analytics world.",
+        "profile_picture": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Jordan_Basketfield.png",
+        "picture_small": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Jordan_Basketfield_Small.png",
+        "nominal_precision": 0.7,
+        "path": "xgb"
+    }
+]
+
+    # {
+    #     "name": "Mystic Meg",
+    #     "description": "Mystic Meg, a renowned British astrologer, stared into her crystal ball. \"Tonight,\" she whispered, \"the stars will align, and the underdogs will prevail.\" Her predictions, based on the alignment of the planets, rarely failed her. As the game unfolded on her old radio, Meg listened intently, a small smile playing on her lips as her prediction came true, reminding her that sometimes, the most unexpected outcomes are the most thrilling.",
+    #     "profile_image": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Mystic_Meg.png",
+    #     "picrture_small": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Mystic_Meg_small.png",
+    #     "nominal_precision": 0.7,
+    #     "path": "xgb"
+    # },
+    # {
+    #     "name": "Paul the Octopus",
+    #     "description": "Paul the Octopus, a German octopus, was an animal oracle who predicted the results of football matches. He lived in a tank at a Sea Life Centre in Oberhausen, Germany. He came to worldwide attention with his accurate predictions in the 2010 World Cup. He correctly predicted the outcomes of all seven of Germany's matches, as well as the final, which Spain won, defeating the Netherlands. Paul achieved renown for his apparent clairvoyance, and some considered him a supernatural creature.",
+    #     "profile_image": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Paul_the_Octopus.png",
+    #     "picrture_small": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/Paul_the_Octopus_small.png",
+    #     "nominal_precision": 0.75,
+    #     "path": "svc"
+    # },
+    # {
+    #     "name": "The Oracle of Delphi",
+    #     "description": "The Oracle of Delphi was the Pythia, the priestess at the Temple of Apollo at Delphi, located on the slopes of Mount Parnassus. The Oracle of Delphi was the most important shrine in all Greece, and in theory all Greeks respected its independence. The oracle is one of the best-known religious institutions of the ancient Greek world. The earliest traditions regarding the oracle of Delphi describe an Earth goddess named Themis, revered as \"the giver of divine law and custom.\" Her successor was Phoebe, another Titaness. The Pythia, was the title of the priestess at the Temple of Apollo at Delphi who served as the oracle, commonly known as the Oracle of Delphi. The name Pythia is derived from Pytho, which in myth was the original name of Delphi. In etymology, the Greeks derived this place name from the verb, pythein (πύθειν, \"to rot\"), which refers to the sickly sweet smell of the decomposition of the body of the monstrous Python after he was slain by Apollo.",
+    #     "profile_image": "https://basketballarkaju.blob.core.windows.net/basteball-public/prediction_models_img/The_Oracle_of_Delphi.png",
+    # }
